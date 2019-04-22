30e419c5dab79a6276103d36030383492c1ebda2
UltranmanShuai <leoshuaigao@gmail.com>
2019-04-22 14:25:27 +1000

    code format

--- notebook/MINST/backward.py
+++ notebook/MINST/backward.py
@@ -15,32 +15,32 @@ MODEL_NAME = "mnist_model"
 
 def backward(mnist):
     x = tf.placeholder(dtype=tf.float32,
-    shape=[None, forward.INPUT_NODE])
+                       shape=[None, forward.INPUT_NODE])
     y_ = tf.placeholder(dtype=tf.float32,
-    shape=[None, forward.OUTPUT_NODE])
-    y = forward.forward(x,REGULARIZER)
+                        shape=[None, forward.OUTPUT_NODE])
+    y = forward.forward(x, REGULARIZER)
 
-    global_step = tf.Variable(0,trainable = False)
+    global_step = tf.Variable(0, trainable=False)
 
-    ce = tf.nn.sparse_softmax_cross_entropy_with_logits(logits = y,
-    labels= tf.argmax(y_,1))
+    ce = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y,
+                                                        labels=tf.argmax(y_, 1))
     cem = tf.reduce_mean(ce)
     loss = cem + tf.add_n(tf.get_collection('losses'))
 
     learning_rate = tf.train.exponential_decay(
-    LEARNING_RATE_BASE,
-    global_step,
-    mnist.train.num_examples/BATCH_SIZE,
-    LEARNING_RATE_DECAY,
-    staircase=True)
+        LEARNING_RATE_BASE,
+        global_step,
+        mnist.train.num_examples / BATCH_SIZE,
+        LEARNING_RATE_DECAY,
+        staircase=True)
 
     train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,
-    global_step=global_step)
+                                                                           global_step=global_step)
 
-    ema = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY,global_step)
+    ema = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
     ema_op = ema.apply(tf.trainable_variables())
-    with tf.control_dependencies([train_step,ema_op]):
-        train_op = tf.no_op(name = 'train')
+    with tf.control_dependencies([train_step, ema_op]):
+        train_op = tf.no_op(name='train')
 
     saver = tf.train.Saver()
 
@@ -48,19 +48,21 @@ def backward(mnist):
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
 
-        for i  in range(STEPS):
+        for i in range(STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
-            _,loss_value,step = sess.run([train_op,loss,global_step],
-            feed_dict={x:xs,y_:ys})
+            _, loss_value, step = sess.run([train_op, loss, global_step],
+                                           feed_dict={x: xs, y_: ys})
             if i % 1000 == 0:
-                print("After %d training step(s)m loss on training batch is %g."%(step,loss_value))
-                saver.save(sess, os.path.join(MODEL_SAVE_PATH,MODEL_NAME),
-                global_step=global_step)
+                print("After %d training step(s)m loss on training batch is %g." % (
+                    step, loss_value))
+                saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME),
+                           global_step=global_step)
 
 
 def main():
-    mnist = input_data.read_data_sets("./data/",one_hot = True)
+    mnist = input_data.read_data_sets("./data/", one_hot=True)
     backward(mnist)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
